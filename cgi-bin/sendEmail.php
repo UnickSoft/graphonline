da71f90c5be4a74b6078e9db580f0df49e95d1e6
Unick Soft <admin@unick-soft.ru>
2017-10-12 23:31:57 +0300

    Fix parameter

--- cgi-bin/sendEmail.php
+++ cgi-bin/sendEmail.php
@@ -7,7 +7,7 @@ $mymail = "soft_support@list.ru";
 
 header('Content-type: text/html; charset=utf-8');
 
-$text = "New Algorithm!\n" . $_POST["text"];
+$text = "New Algorithm!\n" . $_GET["text"];
 
 $headers  = 'MIME-Version: 1.0' . "\r\n";
 $headers .= 'Content-type: text/plain; charset=utf-8' . "\r\n";
