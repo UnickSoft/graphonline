fbfcdee1bdf3f79ff85b88c20465ed80df2f76c3
Unick Soft <admin@unick-soft.ru>
2019-02-15 20:30:41 +0200

    Added facebook page

--- lang/en/contacts.php
+++ lang/en/contacts.php
@@ -1,7 +1,7 @@
 <?php
 
     $g_lang["title_notg"] = "Graph Online feedback";
-    $g_lang["text"] = "<p>Please, send your questions to <a href=\"mailto:admin@unick-soft.ru?subject=GraphOnline%20-%20feedbcak\">admin@unick-soft.ru</a> or <a href=\"mailto:admin@graphonline.ru?subject=Graphonline%20-%20feedback\">admin@graphonline.ru</a></p>\n";
+    $g_lang["text"] = "<p>Please, send your questions to <a href=\"mailto:admin@graphonline.ru?subject=Graphonline%20-%20feedback\">admin@graphonline.ru</a> or look <a href=\"https://www.facebook.com/pg/Graphonline-2230709670325388\" target=\"_blank\">our facebook page</a></p>";
 
     $g_lang["m_title"] = "Graph Online contacts";
     $g_lang["m_keyWords"] = "graph online contacts";
