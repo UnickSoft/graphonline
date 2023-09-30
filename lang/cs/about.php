131f1a0e14fb31f1f47b311e1b93855acc055b18
Oleg Sh <soft_support@list.ru>
2023-09-30 17:23:25 +0200

    Fix translation.

--- lang/cs/about.php
+++ lang/cs/about.php
@@ -1,8 +1,8 @@
 <?php
 
     $g_lang["title_notg"] = "O projektu Graph Online";
-    $g_lang["text"] = "<p>Graph Online byl vytvořen autory aplikace <a href=\"http://grafoanalizator.unick-soft.ru/en/\" style=\"color: rgb(207, 56, 34); margin: 0px;\"><span style=\"color:#000000\">Graphanalyzer</span></a>. Nyní se jedná o online službu pro vzdělávání a podnikání.</p> <p>Big thanks for <a href=\"https://www.facebook.com/mazuchjakub\" target=\"_blank\">Jakub Mazuch</a>, who help us with translation to Czech language.</p>\n";
-
+    $g_lang["text"] = "<p>Graph Online byl vytvořen autory aplikace <a href=\"http://grafoanalizator.unick-soft.ru/en/\" style=\"color: rgb(207, 56, 34); margin: 0px;\"><span style=\"color:#000000\">Graphanalyzer</span></a>. Nyní se jedná o online službu pro vzdělávání a podnikání.</p> <p>Many thanks to <a href=\"https://www.facebook.com/mazuchjakub\" target=\"_blank\">Jakub Mazuch</a> for his help with the translation into Czech.</p>\n";
+                                                                                                                                                                                                                                                                                                  
     $g_lang["m_title"] = "O online službě Graph";
     $g_lang["m_keyWords"] = "graf online, najít nejkratší cestu, vizualizace grafu";
     $g_lang["m_description"] = "O projektu Graph Online";
