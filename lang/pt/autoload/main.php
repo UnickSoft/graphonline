62ee48191e712e57c173f3dad77016070f428d8e
Victor Schmidt <101749261+victorrschmidt@users.noreply.github.com>
2023-10-04 20:22:39 -0300

    Update main.php

--- lang/pt/autoload/main.php
+++ lang/pt/autoload/main.php
@@ -3,6 +3,6 @@
     $g_lang["m_titlePostfix"] = "";
 
     $g_lang["m_title"] = "Graph Online";
-    $g_lang["m_keyWords"] = "Graph Online, find shortest path, visualization";
+    $g_lang["m_keyWords"] = "Graph Online, encontrar menor caminho, visualização";
     $g_lang["m_description"] = "";
 ?>
