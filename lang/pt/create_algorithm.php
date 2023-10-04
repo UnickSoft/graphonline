f7ee679e1c4537a6fa4d689b1cb822b0cb85abbc
Victor Schmidt <101749261+victorrschmidt@users.noreply.github.com>
2023-10-04 20:48:26 -0300

    Update create_algorithm.php

--- lang/pt/create_algorithm.php
+++ lang/pt/create_algorithm.php
@@ -3,10 +3,10 @@
 include ("home.php");
 
 
-    $g_lang["title_notg"] = "Create algorithm for graph";
-    $g_lang["text"] = "<p>Create own algorithm for graph using JavaScript. To learn more <a href=\"http://graphonline.ru/en/wiki/Development/Documentation\">read API documentation</a>.</p>\n";
-    $g_lang["m_title"] = "Create own algorithm on JavaScript for graph online";
-    $g_lang["m_keyWords"] = "graphs, creating of algorithm, JavaScript";
-    $g_lang["m_description"] = "Create algorithm on JavaScript for graph online";
+    $g_lang["title_notg"] = "Criar algoritmo para grafo";
+    $g_lang["text"] = "<p>Crie seu próprio algoritmo para grafo utilizando JavaScript. Para saber mais <a href=\"http://graphonline.ru/en/wiki/Development/Documentation\">leia a documentação da API</a>.</p>\n";
+    $g_lang["m_title"] = "Crie seu próprio algoritmo em JavaScript para o graph online";
+    $g_lang["m_keyWords"] = "grafos, criação de algoritmo, JavaScript";
+    $g_lang["m_description"] = "Crie algoritmo em Javascript para o graph online";
 
 ?>
