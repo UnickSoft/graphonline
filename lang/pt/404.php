ac393a5efc5486b21c6b8425adfafc404a86556f
Victor Schmidt <101749261+victorrschmidt@users.noreply.github.com>
2023-10-04 20:33:13 -0300

    Update 404.php

--- lang/pt/404.php
+++ lang/pt/404.php
@@ -1,7 +1,7 @@
 <?php
 
-    $g_lang["head"] = "Page is not found";
-    $g_lang["message"] = "<p>Sorry, but the page was not found on the server.</p>\n\n<p>Perhaps, these options may&nbsp;help you:</p>\n\n<ul>\n	<li>Go to the <a href=\"/\" title=\"Go to home page\">home page</a></li>\n	<li>Return to the <a href=\"javascript:history.back()\" title=\"Go to previous page\">previous page</a></li>\n</ul>\n";
+    $g_lang["head"] = "Página não encontrada";
+    $g_lang["message"] = "<p>Desculpe, mas a página não foi encontrada no servidor.</p>\n\n<p>Talvez essas opções possam&nbsp;ajudar:</p>\n\n<ul>\n	<li>Ir para a <a href=\"/\" title=\"Ir para a página principal\">página principal</a></li>\n	<li>Voltar para a <a href=\"javascript:history.back()\" title=\"Voltar para a página anterior\">página anterior</a></li>\n</ul>\n";
 
-    $g_lang["m_title"] = "404 Page is not found";
+    $g_lang["m_title"] = "404 Página não encontrada";
 ?>
