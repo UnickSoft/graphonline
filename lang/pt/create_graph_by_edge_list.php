c6a18682f3cd41a46044d8b84e2688feb65cefe3
Victor Schmidt <101749261+victorrschmidt@users.noreply.github.com>
2023-10-04 21:25:06 -0300

    Update create_graph_by_edge_list.php

--- lang/pt/create_graph_by_edge_list.php
+++ lang/pt/create_graph_by_edge_list.php
@@ -1,7 +1,7 @@
 <?php
 
     $g_lang['head_no_tags'] = 'Criar um grafo a partir de uma Lista de Arestas';
-    $g_lang['text'] = '<p>Nessa página você pode entrar &nbsp;lista de arestas e representação gráfica</p>';
+    $g_lang['text'] = '<p>Nesta página você pode criar uma&nbsp;lista de arestas e desenhar o grafo</p>';
     $g_lang['pair_description'] = 'Criar lista de arestas. Cada aresta deve ser colocada em uma nova linha. Use "<b>-</b>" como separados entre os vértices. E.g. <b>1-2</b>. <small>Leia sobre formato estendido abaixo.</small>';    
     $g_lang['plot_graph_button'] = 'Representação gráfica';
     $g_lang['pair_bad_format'] = 'O formato de Lista de Arestas está incorreto. O formato da aresta deve ser: vértice1-vértice2. <small>Leia sobre formato de extensão abaixo.</small>';
