2c8062b1ea61fc51186d5abaf5cd53f93a4dc6a9
Victor Schmidt <101749261+victorrschmidt@users.noreply.github.com>
2023-10-04 21:30:44 -0300

    Update create_graph_by_incidence_matrix.php

--- lang/pt/create_graph_by_incidence_matrix.php
+++ lang/pt/create_graph_by_incidence_matrix.php
@@ -1,18 +1,18 @@
 <?php
 
-    $g_lang["head_no_tags"] = "Criando um grafo a partir de uma matriz de incidência";
+    $g_lang["head_no_tags"] = "Criar um grafo a partir de uma matriz de incidência";
     $g_lang["text"] = "<p>Nesta página, você pode criar uma matriz de incidência e desenhar o grafo</p>";
-    $g_lang["incidence_matrix_description"] = "Entre a matriz de incidência. Use a vírgula \",\" como separador e clique em \"Plotar Grafo\".";
-    $g_lang["incidence_matrix_bad_format"] = "Matriz incorreta. Use vírgula como separador. A matriz deve ser quadrada.";
-    $g_lang["plot_graph_button"] = "Plotar grafo";
-    $g_lang["matrix_matrix_input"] = "Entre como tabela";
-    $g_lang["text_matrix_input"] = "Entre como texto";
-    $g_lang["incidence_matrix_description_matrix"] = "Entre a matriz de incidência. Clique em \"Plotar Grafo\".";
-    $g_lang["add_edge_to_matrix"] = "Insere aresta";
-    $g_lang["add_node_to_matrix"] = "Insere nodo";
+    $g_lang["incidence_matrix_description"] = "Defina a matriz de incidência. Use a vírgula \",\" como separador e clique em \"Representação gráfica\".";
+    $g_lang["incidence_matrix_bad_format"] = "Matriz inválida. Use vírgula \",\" como separador. A matriz deve ser quadrada.";
+    $g_lang["plot_graph_button"] = "Representação gráfica";
+    $g_lang["matrix_matrix_input"] = "Digitar como tabela";
+    $g_lang["text_matrix_input"] = "Digitar como texto";
+    $g_lang["incidence_matrix_description_matrix"] = "Defina a matriz de incidência. Clique em \"Representação gráfica\".";
+    $g_lang["add_edge_to_matrix"] = "Adicionar aresta";
+    $g_lang["add_node_to_matrix"] = "Adicionar vértice";
     $g_lang["m_title"] = "Criando grafo a partir de uma matriz de incidência";
-    $g_lang["m_keyWords"] = "grafo onilne, plotar grafo, criar grafo a partir de uma matriz de incidência, matriz de incidência ";
-    $g_lang["m_description"] = "Crie o grafo online a partir da matriz de incidência e veja o resultado";
+    $g_lang["m_keyWords"] = "graph online, representação gráfica, criar grafo a partir de uma matriz de incidência, matriz de incidência";
+    $g_lang["m_description"] = "Crie o grafo online a partir de uma matriz de incidência e veja o resultado";
 
-    $g_lang["use_ctrl_to_move_cells"] = "Use Ctrl + &#8592; &#8593; &#8594; &#8595; keys to move between cells.";
+    $g_lang["use_ctrl_to_move_cells"] = "Use as teclas Ctrl + &#8592; &#8593; &#8594; &#8595; para mover entre as células.";
 ?>
