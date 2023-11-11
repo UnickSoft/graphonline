7d6de6d6be1017dc18d31b80211e4ad39b2d5d3c
Oleg Sh <soft_support@list.ru>
2023-11-11 16:04:46 +0200

    Removed unnecessary files.

--- script/merge.php
+++ script/merge.php
@@ -1,18 +1,1 @@
-<h1>Update cache</h1>
-
-<?php
-  $cacheList =  array(
-  "/script/pages/editor/api/index.js",
-	"/script/pages/create_graph_by_edge_list/api/index.js", 
-	"/script/pages/create_graph_by_incidence_matrix/api/index.js",
-	"/script/pages/create_graph_by_matrix/api/index.js"
-	);
-
-  echo ("<ul>");
-  foreach ($cacheList as &$page) {
-    $date = date('d.m.Y H:i:s', filemtime("../$page.cache"));
-    echo ("<li><a href='pack.php?target=$page' target='_blank'>$page</a> $date</li>");
-  }
-  echo ("</ul>");
-?>
-
+<h1>Use /admin/page_update_script_cache page to update caches</h1>
