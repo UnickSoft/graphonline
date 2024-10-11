037d98a5eb2055dab2e8fa59e30d1e3474bb64b9
/usr/bin/nano <support@beget.ru>
2024-10-11 21:09:59 +0300

    Update wiki-s

---
+++ en/wiki/wiki.d/C.C
@@ -0,0 +1,16 @@
+version=pmwiki-2.2.75 ordered=1 urlencoded=1
+agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36
+author=ZAP
+charset=UTF-8
+csum=ZAP
+ctime=1707846571
+host=188.255.35.115
+name=C.C
+rev=1
+targets=
+text=
+time=1707846571
+author:1707846571=ZAP
+csum:1707846571=ZAP
+diff:1707846571:1707846571:minor=
+host:1707846571=188.255.35.115
